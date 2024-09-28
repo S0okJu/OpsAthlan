6cfe8b43c9e39668ef860f23bd0b6c137a787b67
S0okJu <damekyung98@gmail.com>
2024-09-28 11:34:36 +0900

    [sc/2] Async

---
+++ scenario_02/spring_advanced_echo/src/main/java/org/opsathlan/spring_advanced_echo/configuration/AsyncConfig.java
@@ -0,0 +1,9 @@
+package org.opsathlan.spring_advanced_echo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
