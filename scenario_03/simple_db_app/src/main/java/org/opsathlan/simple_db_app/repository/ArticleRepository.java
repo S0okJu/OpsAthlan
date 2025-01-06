9f996a96af5b65af86388ae3d51093187c13bc46
S0okJu <damekyung98@gmail.com>
2025-01-06 22:15:51 +0900

    [Chore] 불필요한 Springboot 앱 삭제

--- scenario_03/simple_db_app/src/main/java/org/opsathlan/simple_db_app/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package org.opsathlan.simple_db_app.repository;
-
-import org.opsathlan.simple_db_app.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
